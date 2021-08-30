c9e28f6a79fe169456a4b23bc1679b9eb1118948
sakeer.pmt@gmail.com <sakeer.pmt@gmail.com>
2021-08-30 17:04:52 -0500

    Add project files.

---
+++ SweetTreatsStore/Model/CourierModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweetTreatsStore.Model
+{
+    public class CourierModel
+    {
+        public int cId { get; set; }
+        public string name { get; set; }
+        public DateTime workingStartHours { get; set; }
+        public DateTime workingEndHours { get; set; }
+        public int deliveryRange { get; set; }
+        public double charges { get; set; }
+        public bool hasRefrigirated { get; set; }
+    }
+}
---
+++ SweetTreatsStore/Program.cs
@@ -0,0 +1,85 @@
+﻿using SweetTreatsStore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweetTreatsStore
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime shipTime = Convert.ToDateTime("12:00");
+            int deliveryRanage = 3;
+            bool hasRefrigerator = true;
+
+            List<CourierModel> obj =  GetCheapestCourier(shipTime, deliveryRanage, hasRefrigerator);
+
+            if (obj.Count == 0) Console.WriteLine("No data found..");
+            else
+            {
+                foreach (var row in obj)
+                {
+                    Console.WriteLine("Name:" + row.name + " " + "Charges:" + row.charges + " " + "Range:" + row.deliveryRange +"miles");
+                }
+            }
+            Console.ReadLine();
+        }
+
+        public static List<CourierModel> GetCheapestCourier(DateTime shippedTime, int distance, bool hasRefrigerator)
+        {
+            List<CourierModel> data = AddDataToModel();
+            var result = data.Where(c =>
+             c.workingStartHours <= shippedTime &&
+             c.workingEndHours >= shippedTime &&
+             c.deliveryRange >= distance &&
+             c.hasRefrigirated == hasRefrigerator
+             )
+               .OrderBy(o=>o.charges)
+                .ToList();
+
+            return result.ToList();
+        }
+        
+        //add data
+        public static List<CourierModel> AddDataToModel()
+        {
+            List<CourierModel> cm = new List<CourierModel>();
+            cm.Add(new CourierModel()
+            {
+                cId = 1,
+                name = "Bobby",
+                deliveryRange = 5,
+                workingStartHours = Convert.ToDateTime("9:00"),
+                workingEndHours = Convert.ToDateTime("13:00"),
+                charges = 1.75,
+                hasRefrigirated = true
+            });
+            cm.Add(new CourierModel()
+            {
+                cId = 2,
+                name = "Martin ",
+                deliveryRange = 3,
+                workingStartHours = Convert.ToDateTime("9:00"),
+                workingEndHours = Convert.ToDateTime("17:00"),
+                charges = 1.50,
+                hasRefrigirated = false
+            });
+            cm.Add(new CourierModel()
+            {
+                cId = 3,
+                name = "Geoff ",
+                deliveryRange = 4,
+                workingStartHours = Convert.ToDateTime("10:00"),
+                workingEndHours = Convert.ToDateTime("16:00"),
+                charges = 2.0,
+                hasRefrigirated = true
+            });
+            return cm;
+        }
+    }
+
+   
+}
